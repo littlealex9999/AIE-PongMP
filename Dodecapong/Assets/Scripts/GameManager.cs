9e3393e1217f7008744b001d0eef62c0eedc0cca
Alex G <littlealex9999@gmail.com>
2023-07-24 10:29:16 +1000

    change map circle calculation

---
+++ Dodecapong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Map map;
+    Ball ball;
+
+    public int playerCount;
+
+    [ColorUsage(true, true)] public List<Color> playerEmissives = new List<Color>();
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void BuildGameBoard()
+    {
+        
+    }
+}
---
+++ Dodecapong/Assets/Scripts/Map.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Map : MonoBehaviour
+{
+    public float mapRadius;
+
+    public int lineStepCount;
+
+    public float angleOffset;
+
+    public LineRenderer lr;
+
+    private void OnValidate()
+    {
+        if (!lr || lineStepCount < 1) return;
+
+        lr.positionCount = lineStepCount;
+        Vector3 startPos = GetTargetPointInCircleLocal(angleOffset);
+        lr.SetPosition(0, startPos + transform.position);
+
+        Quaternion targetRotation = Quaternion.identity;
+        Quaternion rotationPerSegment = Quaternion.Euler(0, 0, 360 / lineStepCount);
+
+        for (int i = 1; i < lineStepCount; i++) {
+            targetRotation *= rotationPerSegment;
+            lr.SetPosition(i, targetRotation * startPos + transform.position);
+        }
+    }
+
+    public Vector3 GetTargetPointInCircle(float angle)
+    {
+        return transform.position + Quaternion.Euler(0, 0, angle) * transform.up * mapRadius;
+    }
+
+    public Vector3 GetTargetPointInCircleLocal(float angle)
+    {
+        return Quaternion.Euler(0, 0, angle) * transform.up * mapRadius;
+    }
+}
