df799ab110419f6589645388805835881bb83054
FinnDavis <s213398@students.aie.edu.au>
2023-08-28 11:10:59 +1000

    ball collision and event

--- Dodecapong/Assets/Scripts/Ball.cs
+++ Dodecapong/Assets/Scripts/Ball.cs
@@ -17,11 +17,18 @@ public class Ball : MonoBehaviour
 
     float distFromCenter { get { return Vector2.Distance(transform.position, Vector2.zero); } }
 
+
     // Start is called before the first frame update
     void Start()
     {
         collider = GetComponent<PongCollider>();
+        collider.OnPaddleCollision += OnPaddleCollision;
         GameManager.instance.OnGameStateChange += OnGameStateChanged;
+    }
+
+    private void OnPaddleCollision(PongCollider other)
+    {
+        EventManager.instance?.ballHitEvent?.Invoke();
     }
 
     private void OnGameStateChanged()
