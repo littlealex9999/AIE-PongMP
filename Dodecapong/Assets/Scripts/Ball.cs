7407348a6d6acfbde3320536215c08a3336e4672
FinnDavis <s213398@students.aie.edu.au>
2023-09-04 14:56:15 +1000

    explosions

--- Dodecapong/Assets/Scripts/Ball.cs
+++ Dodecapong/Assets/Scripts/Ball.cs
@@ -22,6 +22,8 @@ public class Ball : MonoBehaviour
 
     bool held;
 
+    public ParticleSystem collisionEffect; 
+
     void Awake()
     {
         collider = GetComponent<PongCircleCollider>();
@@ -33,6 +35,7 @@ public class Ball : MonoBehaviour
     {
         if (other.gameObject.TryGetComponent(out Player player))
         {
+            collisionEffect.Play();
             if (player.grabbing)
             {
                 StartCoroutine(player.GrabRoutine());
@@ -95,6 +98,7 @@ public class Ball : MonoBehaviour
 
     private void BounceOnBounds()
     {
+        collisionEffect.Play();
         Vector2 shieldNormal = (Vector3.zero - transform.position).normalized;
         Bounce(shieldBounceTowardsCenterBias, shieldNormal);
     }
