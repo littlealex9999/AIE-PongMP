62147e511363d15a4491c4a2c189dc9e92ba6ac0
Alex G <littlealex9999@gmail.com>
2023-09-13 11:54:06 +1000

    add credit texture change template

--- Dodecapong/Assets/Scripts/CreditsTextureSwap.cs
+++ Dodecapong/Assets/Scripts/CreditsTextureSwap.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class CreditsTextureSwap : MonoBehaviour
 {
     public Material pinArtMaterial;
     public float duration = 2.0f;
     float elapsed = 0.0f;
-    float completion;
 
     private void Update()
     {
         if (elapsed < duration) {
             elapsed += Time.deltaTime;
+            if (elapsed > duration) elapsed = duration;
 
+            SetLerp(elapsed / duration);
         }
     }
 
@@ -22,9 +24,16 @@ public class CreditsTextureSwap : MonoBehaviour
     {
         // apply old second texture to first texture
         // reset lerp to 0
+        //pinArtMaterial.SetTexture(, texture);
+        //pinArtMaterial.SetFloat(, value);
 
         pinArtMaterial.SetTexture("_Render_Texture", texture);
 
         elapsed = 0.0f;
+    }
+
+    void SetLerp(float value)
+    {
+        //pinArtMaterial.SetFloat(, value);
     }
 }
