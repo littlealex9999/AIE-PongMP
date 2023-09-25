6acea46ff060e171cf2adacc61f1712244429538
Alex G <littlealex9999@gmail.com>
2023-09-25 15:58:20 +1000

    add menu correct values on load

--- Dodecapong/Assets/Scripts/UI/UISliderPassthrough.cs
+++ Dodecapong/Assets/Scripts/UI/UISliderPassthrough.cs
@@ -2,11 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class UISliderPassthrough : MonoBehaviour
 {
+    public Slider slider;
     public float[] values;
     public UnityEvent<float> UnityEvent;
+
+    private void Awake()
+    {
+        if (!slider) slider = GetComponent<Slider>();
+    }
+
+    public void SetSliderToApproximate(float val)
+    {
+        for (int i = 0; i < values.Length; i++) {
+            if (i > 0) {
+                if (values[i - 1] >= val) {
+                    continue;
+                }
+            }
+
+            if (i < values.Length - 1) {
+                if (values[i + 1] <= val) {
+                    continue;
+                }
+            }
+
+            if (!slider) slider = GetComponent<Slider>();
+            slider.value = i;
+            break;
+        }
+    }
 
     public void ApplyValue(int index)
     {
